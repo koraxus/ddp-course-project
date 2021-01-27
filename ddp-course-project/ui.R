1deaf90c00d37febc4d31adedf8775526602e247
Truong Tran-old <koraxus@Truong-Trans-MacBook-Pro.local>
2021-01-26 22:13:19 -0700

    refine

--- ddp-course-project/ui.R
+++ ddp-course-project/ui.R
@@ -13,24 +13,26 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("The predicted stopping distance of a 1920's motor vehicle"),
+  titlePanel("Predict the stopping distance of a 1920's motor vehicle"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
+      p("Use the slider bar to select the speed of the car"),
+      p("The flux capacitor is activated at 88 mph"),
        sliderInput("sliderSpeed",
                    "What is the speed of the car (mph)?",
                     min = 4,
                     max = 150,
                     value = 15),
-       checkboxInput("showModel1","Show/Hide Model 1",value=TRUE)
+       checkboxInput("showModel1","Show/Hide Model",value=TRUE)
        
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
        plotOutput("plot1"),
-       h3("Predicted Stopping distance in ft from Model 1:"),
+       h3("Predicted Stopping distance in ft:"),
        textOutput("pred1")
        
     )
